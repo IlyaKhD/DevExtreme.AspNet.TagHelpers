b0dd2f82a6af98bb88c5543d2c7108610bfafe5c
Ilya Kh.d <kh.d@bk.ru>
2015-12-29 16:59:49 +0300

    PropTypeInfo override table initialization refactored

--- DevExtreme.AspNet.TagHelpers.Generator/PropTypeInfo.cs
+++ DevExtreme.AspNet.TagHelpers.Generator/PropTypeInfo.cs
@@ -19,78 +19,7 @@ namespace DevExtreme.AspNet.TagHelpers.Generator {
             SPECIAL_RAW_STRING = "sp_raw_string",
             SPECIAL_DOM_TEMPLATE = "sp_dom_template";
 
-        static readonly IDictionary<string, string> _overrideTable = new Dictionary<string, string> {
-            { "DevExtreme.AspNet.TagHelpers.Data.Datasource.Field.FilterValues",CLR_ARRAY_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.Data.Datasource.Field.SortBySummaryPath",CLR_ARRAY_STRING },
-            { "DevExtreme.AspNet.TagHelpers.Data.Datasource.Filter",CLR_ARRAY_OBJECT },
-
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Categories",CLR_ARRAY_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.ConstantLine.Value",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Max",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Min",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.MinorTickInterval",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Strip.EndValue",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Strip.StartValue",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.TickInterval",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.CommonSeriesSettings.Label.Position",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.DataPrepareSettings.SortingMethod",CLR_BOOL },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.Series.Label.Position",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.Tooltip.Container",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Categories",CLR_ARRAY_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.ConstantLine.Value",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Max",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Min",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.MinorTickInterval",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Strip.EndValue",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Strip.StartValue",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.TickInterval",CLR_OBJECT },
-
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.CalculateCellValue",SPECIAL_RAW_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.CalculateDisplayValue",SPECIAL_RAW_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.CalculateGroupValue",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.CalculateSortValue",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.EditorOptions",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.FilterValues",CLR_ARRAY_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.FormItem",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.Lookup.DisplayExpr",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.ValidationRules",CLR_ARRAY_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Editing.Form",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.FilterRow.OperationDescriptions",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Height",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Pager.AllowedPageSizes",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Pager.Visible",CLR_BOOL },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Scrolling.UseNative",CLR_BOOL },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.SelectedRowKeys",CLR_ARRAY_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Width",CLR_STRING },
-
-            { "DevExtreme.AspNet.TagHelpers.dxPieChart.CommonSeriesSettings.Label.Position",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxPieChart.Series.Label.Position",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxPieChart.Tooltip.Container",CLR_STRING },
-
-            { "DevExtreme.AspNet.TagHelpers.dxPivotGrid.Height",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxPivotGrid.Scrolling.UseNative",CLR_BOOL },
-            { "DevExtreme.AspNet.TagHelpers.dxPivotGrid.Width",CLR_STRING },
-
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Chart.CommonSeriesSettings.Label.Position",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Chart.DataPrepareSettings.SortingMethod",CLR_BOOL },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Chart.Series.Label.Position",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.Categories",CLR_ARRAY_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.EndValue",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.TickInterval",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.MinorTickInterval",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.StartValue",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.SelectedRange.EndValue",CLR_OBJECT },
-            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.SelectedRange.StartValue",CLR_OBJECT },
-
-            { "DevExtreme.AspNet.TagHelpers.dxScheduler.CurrentDate",CLR_DATE },
-            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Groups",CLR_ARRAY_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Height",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Resource.DisplayExpr",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Resource.ValueExpr",CLR_STRING },
-            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Width",CLR_STRING },
-
-            { "DevExtreme.AspNet.TagHelpers.dxSparkline.Tooltip.Container",CLR_STRING }
-        };
+        static readonly IDictionary<string, string> _overrideTable = InitOverrideTable();
 
         public readonly string ClrType;
         public readonly bool IsDomTemplate;
@@ -172,6 +101,79 @@ namespace DevExtreme.AspNet.TagHelpers.Generator {
 
             return type;
         }
+
+        static IDictionary<string, string> InitOverrideTable() => new Dictionary<string, string> {
+            { "DevExtreme.AspNet.TagHelpers.Data.Datasource.Field.FilterValues", CLR_ARRAY_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.Data.Datasource.Field.SortBySummaryPath", CLR_ARRAY_STRING },
+            { "DevExtreme.AspNet.TagHelpers.Data.Datasource.Filter", CLR_ARRAY_OBJECT },
+
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Categories", CLR_ARRAY_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.ConstantLine.Value", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Max", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Min", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.MinorTickInterval", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Strip.EndValue", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.Strip.StartValue", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ArgumentAxis.TickInterval", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.CommonSeriesSettings.Label.Position", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.DataPrepareSettings.SortingMethod", CLR_BOOL },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.Series.Label.Position", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.Tooltip.Container", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Categories", CLR_ARRAY_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.ConstantLine.Value", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Max", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Min", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.MinorTickInterval", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Strip.EndValue", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.Strip.StartValue", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxChart.ValueAxis.TickInterval", CLR_OBJECT },
+
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.CalculateCellValue", SPECIAL_RAW_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.CalculateDisplayValue", SPECIAL_RAW_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.CalculateGroupValue", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.CalculateSortValue", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.EditorOptions", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.FilterValues", CLR_ARRAY_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.FormItem", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.Lookup.DisplayExpr", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Column.ValidationRules", CLR_ARRAY_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Editing.Form", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.FilterRow.OperationDescriptions", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Height", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Pager.AllowedPageSizes", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Pager.Visible", CLR_BOOL },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Scrolling.UseNative", CLR_BOOL },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.SelectedRowKeys", CLR_ARRAY_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxDataGrid.Width", CLR_STRING },
+
+            { "DevExtreme.AspNet.TagHelpers.dxPieChart.CommonSeriesSettings.Label.Position", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxPieChart.Series.Label.Position", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxPieChart.Tooltip.Container", CLR_STRING },
+
+            { "DevExtreme.AspNet.TagHelpers.dxPivotGrid.Height", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxPivotGrid.Scrolling.UseNative", CLR_BOOL },
+            { "DevExtreme.AspNet.TagHelpers.dxPivotGrid.Width", CLR_STRING },
+
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Chart.CommonSeriesSettings.Label.Position", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Chart.DataPrepareSettings.SortingMethod", CLR_BOOL },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Chart.Series.Label.Position", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.Categories", CLR_ARRAY_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.EndValue", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.TickInterval", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.MinorTickInterval", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.Scale.StartValue", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.SelectedRange.EndValue", CLR_OBJECT },
+            { "DevExtreme.AspNet.TagHelpers.dxRangeSelector.SelectedRange.StartValue", CLR_OBJECT },
+
+            { "DevExtreme.AspNet.TagHelpers.dxScheduler.CurrentDate", CLR_DATE },
+            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Groups", CLR_ARRAY_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Height", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Resource.DisplayExpr", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Resource.ValueExpr", CLR_STRING },
+            { "DevExtreme.AspNet.TagHelpers.dxScheduler.Width", CLR_STRING },
+
+            { "DevExtreme.AspNet.TagHelpers.dxSparkline.Tooltip.Container", CLR_STRING }
+        };
     }
 
 }
